041436f886e959b2c0c788d2c76675bff0baeee7
Joshua <tantohyung@gmail.com>
2016-02-04 22:21:53 +0800

    routing set up

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,23 @@
 
 module.exports = function(app, db) {
 
+	app.route('/new*')
+		.get(function(req,res){
+			res.sendFile(process.cwd() + '/public/input-invalid.html');
+			console.log(req.path);
+			if (req.query.allow){
+				console.log(req.query.allow);	
+			}			
+		});
+		
+	app.route('/:short')
+		.get(function(req,res){
+			res.send(req.params.short);
+		});
+
 	app.route('/')
 		.get(function(req, res){
 			res.sendFile(process.cwd() + '/public/index.html');
-		});
+		});		
 
 }
