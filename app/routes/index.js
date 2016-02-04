b84ad772cd0bd8ac0f146d76b9084cf98a2e1a8e
Joshua <tantohyung@gmail.com>
2016-02-04 21:12:41 +0800

    Intial page and app structure set up

---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(app, db) {
+
+	app.route('/')
+		.get(function(req, res){
+			res.sendFile(process.cwd() + '/public/index.html');
+		});
+
+}
--- server.js
+++ server.js
@@ -2,17 +2,26 @@
 
 var express = require('express');
 var mongo = require('mongodb').MongoClient;
+var routes = require('./app/routes/index.js');
 
 var app = express();
-//var mongoURL;
+var mongoURL = "mongodb://heroku_12527h59:te48kkcge8h8fs5jejn33k2onk@ds055485.mongolab.com:55485/heroku_12527h59";
 var port = process.env.PORT || 3000;
 
-app.route('/')
-	.get(function(req, res){
-		res.send('hello world');
+mongo.connect(mongoURL, function(err, db){
+
+	if(err) {
+		throw err;
+	} else {
+		console.log("connecting to Mongo database...");
+	}
+
+	routes(app,db);
+
+	app.listen(port, function(){
+		console.log("listening to port " + port + "...");
 	});
 
-
-app.listen(port, function(){
-	console.log("listening to port " + port + "...");
 });
+
+
