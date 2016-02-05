9b4e659df6533a5f8e065237cda6d84dfbc6136d
Joshua <tantohyung@gmail.com>
2016-02-06 00:03:59 +0800

    Insert entries correct

--- app/controllers/input-handler.server.js
+++ app/controllers/input-handler.server.js
@@ -8,16 +8,27 @@ var InputHandler = function(app,db) {
 		var unfilteredStr = req.path;
 		var url = unfilteredStr.substring(5);		
 
-		collection.count({}, function(err, count){
 
+
+		collection.count({}, function(err, count){
 			if (err) {
 				throw err;
 			}
-
 			var nextIndex = count + 1;
 			var shortenUrl = 'http://' + req.headers.host + '/' + nextIndex;
-			var urlPair = {original_url: url, short_url: shortenUrl};
-			res.json(urlPair);
+			var urlPair = {_id: nextIndex, original_url: url, short_url: shortenUrl};
+
+			collection.insert(urlPair, function(err){
+				if (err) {
+					throw err;
+				}
+				collection.findOne({'_id': nextIndex}, {'_id':false}, function(err, doc){
+					if (err) {
+						throw err;
+					}
+					res.json(doc);
+				});
+			});		
 		});
 	}
 
